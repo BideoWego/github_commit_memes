36dd0fc9a4242a63276e7efa77dd14266e13a71c
Bideo Wego <bideowego@gmail.com>
2018-04-18 17:17:30 -0400

    Move commit excerpt code to view helper

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -1,2 +1,12 @@
 module CommitsHelper
+  def commit_excerpt(commit, mark: false)
+    sanitize(
+      excerpt(
+        highlight(commit.message, commit.q.split(' ')),
+        '</mark>',
+        radius: 100
+      ),
+      tags: mark ? %w(mark) : nil
+    )
+  end
 end
